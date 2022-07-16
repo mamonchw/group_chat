5773922c2e3bc007c2147e85545c1619c9128684
mamonchw <87112380+mamonchw@users.noreply.github.com>
2022-07-16 21:09:56 +0530

    Update description.dart

--- lib/part1/description.dart
+++ lib/part1/description.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class NotiDes extends StatelessWidget {
+class NotiDes extends StatefulWidget {
   final String title;
   final String des;
   const NotiDes({required this.title, required this.des, Key? key})
       : super(key: key);
 
+  //SecondScreen({Key key, @required this.text}) : super(key: key);
+
+  @override
+  State<NotiDes> createState() => _NotiDesState();
+}
+
+class _NotiDesState extends State<NotiDes> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         backgroundColor: Colors.indigoAccent,
       ),
       body: Column(
@@ -21,18 +28,16 @@ class NotiDes extends StatelessWidget {
             height: size.height / 30,
           ),
           Center(
-              child: Flexible(
-                  child: Text(
-            des,
+              child: Text(
+            widget.des,
             style: TextStyle(fontSize: 16),
-          ))),
+          )),
           SizedBox(
             height: size.height / 5,
           ),
           Center(
-              child: Flexible(
-            child: Text("Notification is coming from Firebase Cloud Firestore"),
-          ))
+              child:
+                  Text("Notification is coming from Firebase Cloud Firestore"))
         ],
       ),
     );
